d6eb86470a40d6e2a0db6e04a2a540ec5b054c7f
Arthur Fucher <arthurfucher@gmail.com>
2014-12-25 23:56:22 -0200

    Added mongodb supoot #1

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 var net = require('net');
 var SyslogParser = require("./parser/SyslogParser");
+var MongoClient = require('mongodb').MongoClient;
+var useMongo = false;
+
+//MongoDB connection
+var url = 'mongodb://localhost:27017/syslog';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+	
+	useMongo = true;
+  MongoDB = db;
+	
+});
 
 var server = net.createServer(function (socket) {
 	console.log("connection...");
 	socket.on('data', function(data) {
 		try {
-			console.log(SyslogParser.parse(data.toString()));
+			var info = SyslogParser.parse(data.toString());
+			if(useMongo){
+				var coll = MongoDB.collection('logs');
+				coll.insert(info, function(err, result) {
+
+				});
+			}
 		} catch (ex) {
 			console.log("ERR")//console.log(ex);
 		}
 		
 	});
 });
+
 server.listen(8083);
